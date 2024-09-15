68ef2efdba4abf3a98d7f88a8c193389cfed5173
saucepoint <saucepoint@protonmail.com>
2024-09-15 11:31:18 -0400

    wip

--- rebate-signer/index.ts
+++ rebate-signer/index.ts
@@ -8,8 +8,10 @@ const publicClient = createPublicClient({
 });
 
 Bun.serve({
-  fetch(req) {
-    calculateRebate(publicClient, "0x0" as `0x${string}`);
-    return new Response("Bun!");
+  async fetch(req) {
+    const txnHash =
+      "0x7126b7a4cd438c366824350d5334baac1f2dd4f9c71382629955b03ef1829495" as `0x${string}`;
+    const gasToRebate = await calculateRebate(publicClient, txnHash);
+    return new Response(String(gasToRebate));
   },
 });
--- rebate-signer/src/rebate.ts
+++ rebate-signer/src/rebate.ts
@@ -1,4 +1,13 @@
-import type { Client, PublicClient, TransactionReceipt } from "viem";
+import {
+  parseAbi,
+  parseEventLogs,
+  type PublicClient,
+  type TransactionReceipt,
+} from "viem";
+
+const abi = parseAbi([
+  "event Swap(bytes32 indexed id, address indexed sender, int128 amount0, int128 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick, uint24 fee)",
+]);
 
 export function getGasPerSwap(): bigint {
   return BigInt(0);
@@ -12,18 +21,26 @@ export function getUNIFromETHAmount(ethAmount: bigint): bigint {
   return BigInt(0);
 }
 
-export function calculateRebate(
+/// @dev rebates are only calculated for the first swap router
+export async function calculateRebate(
   client: PublicClient,
   txnHash: `0x${string}`
-): bigint {
-  client
+): Promise<bigint> {
+  return client
     .getTransactionReceipt({ hash: txnHash })
     .then((receipt: TransactionReceipt) => {
-      receipt.logs.forEach((log) => {
-        console.log(log);
+      const gasPrice: bigint = receipt.effectiveGasPrice; // TODO: how do we excluse priority gas, otherwise miners will pay themselves
+      const swapEvents = parseEventLogs({
+        abi: abi,
+        logs: receipt.logs,
+      }).filter((log) => log.eventName === "Swap");
 
-        // for each swap event
+      // iterate each swap event, calculating the rebate for the sender (swap router)
+      let gasToRebate: bigint = 0n;
+      swapEvents.forEach((swapEvent) => {
+        // const { id, sender } = swapEvent.args;
+        gasToRebate += 100_000n;
       });
+      return gasToRebate;
     });
-  return BigInt(0);
 }
