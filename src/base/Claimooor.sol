5553c0af5d3b28c5d668d981d78c037d71e28e27
saucepoint <saucepoint@protonmail.com>
2024-09-15 17:43:28 -0400

    refactoring the script to allow for multiple swaps

---
+++ src/base/Claimooor.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {SignatureRebates} from "../SignatureRebates.sol";
+
+abstract contract Claimooor {
+    SignatureRebates public immutable rebates;
+
+    constructor(SignatureRebates _rebates) {
+        rebates = _rebates;
+    }
+
+    function claimRebate(
+        uint256 campaignId,
+        address recipient,
+        uint256 amount,
+        bytes32 transactionHash,
+        uint256 amountMax,
+        bytes calldata signature
+    ) external {
+        rebates.claim(campaignId, recipient, amount, transactionHash, amountMax, signature);
+    }
+}
