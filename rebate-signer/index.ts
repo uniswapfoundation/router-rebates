7a0911b2e2b8023a02ff0df81c56dd3018a6d43f
saucepoint <saucepoint@protonmail.com>
2024-09-24 20:55:25 +0800

    wip support for batch claims

--- rebate-signer/index.ts
+++ rebate-signer/index.ts
@@ -1,6 +1,6 @@
 import { createPublicClient, http } from "viem";
 import { anvil } from "viem/chains";
-import { main } from "./src/main";
+import { batch, single } from "./src/main";
 
 const publicClient = createPublicClient({
   chain: anvil, // TODO: Use the correct chain
@@ -12,18 +12,27 @@ Bun.serve({
     const url = new URL(req.url);
     const paths = url.pathname.split("/");
 
+    if (paths.length == 2 && paths[1] == "sign") {
+      const campaignId = BigInt(url.searchParams.get("campaignId")!);
+      const txnHashes = url.searchParams.getAll(
+        "txnHashes[]"
+      ) as `0x${string}`[];
+
+      batch(publicClient, campaignId, txnHashes);
+    }
     if (paths.length == 3) {
       const chainId = paths[1];
       const txnHash = paths[2] as `0x${string}`;
       if (chainId === "1") {
         return new Response("Not supported yet");
       } else if (chainId === "31337") {
-        return Response.json(await main(publicClient, txnHash));
+        return Response.json(await single(publicClient, txnHash));
       } else {
         return new Response("Invalid network");
       }
     } else {
       // TODO: proper 404?
+      console.log(url.searchParams.getAll("tags[]"));
       return new Response(
         "Invalid URL. Must be /<chaind_id>/<transaction_hash>"
       );
--- rebate-signer/src/signer.ts
+++ rebate-signer/src/signer.ts
@@ -6,6 +6,12 @@ const types = {
     { name: "transactionHash", type: "bytes32" },
     { name: "amountMax", type: "uint256" },
   ],
+  ClaimableBatch: [
+    { name: "campaignId", type: "uint256" },
+    { name: "referrer", type: "address" },
+    { name: "transactionHashes", type: "bytes32[]" },
+    { name: "amount", type: "uint256" },
+  ],
 };
 
 export async function sign(
@@ -30,6 +36,35 @@ export async function sign(
       referrer: referrer,
       transactionHash: txnHash,
       amountMax: amountMax,
+    },
+  });
+  return signature;
+}
+
+export async function signBatch(
+  campaignId: bigint,
+  referrer: `0x${string}`,
+  txnHashes: `0x${string}`[],
+  amount: bigint
+): Promise<`0x${string}`> {
+  const account = privateKeyToAccount(
+    process.env.ETH_PRIVATE_KEY as `0x${string}`
+  );
+
+  const signature = await account.signTypedData({
+    domain: {
+      name: process.env.REBATE_CONTRACT_NAME,
+      version: process.env.REBATE_CONTRACT_VERSION,
+      chainId: 31337,
+      verifyingContract: process.env.REBATE_ADDRESS as `0x${string}`,
+    },
+    types: types,
+    primaryType: "ClaimableBatch",
+    message: {
+      campaignId: campaignId,
+      referrer: referrer,
+      transactionHashes: txnHashes,
+      amount: amount,
     },
   });
   return signature;
