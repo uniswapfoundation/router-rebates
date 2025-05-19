af43a828629221a8e2b555fe226c93cddc7440b9
saucepoint <saucepoint@protonmail.com>
2025-05-19 11:29:48 -0400

    early-return on no rebates

--- signer/src/api/util/main.ts
+++ signer/src/api/util/main.ts
@@ -1,4 +1,4 @@
-import type { Address, PublicClient } from "viem";
+import { zeroAddress, type Address, type PublicClient } from "viem";
 import { calculateRebate } from "./rebate";
 import { getRebateClaimer, sign } from "./signer";
 
@@ -20,6 +20,19 @@ export async function batch(
     (total: bigint, data) => total + data.gasToRebate,
     0n
   );
+
+  // no rebates were found from the transaction hashes provided
+  // early-return to avoid `getRebateClaimer` call and signature generation
+  if (amount === 0n) {
+    return {
+      claimer: zeroAddress,
+      signature: "0x0",
+      amount: "0",
+      startBlockNumber: "0",
+      endBlockNumber: "0",
+    };
+  }
+
   const beneficiary: `0x${string}` = result[0].beneficiary;
   const claimer = await getRebateClaimer(publicClient, beneficiary);
   const startBlockNumber = result.reduce(
